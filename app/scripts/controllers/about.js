78febd2be7f1c8aae1efb49466f3cb66fc551e83
jaime-e <jechegarayz@gmail.com>
2016-08-16 17:40:52 -0300

    [init]

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name remittanceAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the remittanceAppApp
+ */
+angular.module('remittanceAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name remittanceAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the remittanceAppApp
+ */
+angular.module('remittanceAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
