02c3e25dd4a0b618839fa4f0eb494e3ba483bab8
jaime-e <jechegarayz@gmail.com>
2016-08-17 12:39:14 -0300

    [task] rename app

--- Gruntfile.js
+++ Gruntfile.js
@@ -338,7 +338,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'remittanceAppApp',
+          module: 'remittanceApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('remittanceAppApp'));
+  beforeEach(module('remittanceApp'));
 
   var AboutCtrl,
     scope;
