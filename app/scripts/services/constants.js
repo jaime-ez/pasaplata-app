6523ad10c4ae5734868dc376b4496d577339ac39
jaime-e <jechegarayz@gmail.com>
2016-08-17 12:48:01 -0300

    [task] lodash as constant

---
+++ app/scripts/services/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name remittanceApp.constants
+ * @description
+ * # constants
+ * Constant in the remittanceApp.
+ */
+angular.module('remittanceApp')
+  .constant('_', window._);
