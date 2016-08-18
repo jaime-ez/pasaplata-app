7221b2daeda5032f1261ddbf7d21f91081bb983b
jaime-e <jechegarayz@gmail.com>
2016-08-18 16:15:44 -0300

    [feature] colombia banks and bank account types constant

--- app/scripts/services/constants.js
+++ app/scripts/services/constants.js
@@ -7,5 +7,39 @@
  * # constants
  * Constant in the remittanceApp.
  */
+var colombiaBanks = [
+  'AV Villas',
+  'Bancamía',
+  'Banagrario',
+  'Bancolombia',
+  'Banco Compartir',
+  'Banco Cooperativo',
+  'Banco Coomeva',
+  'Banco Corpbanca',
+  'Banco de Bogotá',
+  'Banco de Occidente',
+  'Banco Falabella',
+  'Banco Finandina',
+  'Banco Mundo Mujer',
+  'Banco Multibank',
+  'Banco Pichincha',
+  'Banco Popular',
+  'Banco Procredit',
+  'Banco Santander',
+  'Banco WWB',
+  'BBVA Colombia',
+  'Caja Social BCSC',
+  'Citibank Colombia',
+  'Colpatria',
+  'Davivienda',
+  'GNB Sudameris'
+];
+
+var colombiaBankAccountTypes = [
+  'Cuenta Corriente', 'Cuenta Ahorro'
+]
+
 angular.module('remittanceApp')
-  .constant('_', window._);
+  .constant('_', window._)
+  .constant('COLOMBIA_BANKS', colombiaBanks)
+  .constant('COLOMBIA_BANK_ACCOUNT_TYPES', colombiaBankAccountTypes);
