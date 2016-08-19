0d14095cbfde5dd9926fb4f39ef7aaf49147ee51
jaime-e <jechegarayz@gmail.com>
2016-08-19 10:05:24 -0300

    [feature]set remittance info form

--- app/scripts/controllers/send.js
+++ app/scripts/controllers/send.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var createRemittanceUrl = 'https://gateway.test.dinex.cl/public/remittance/create';
+
 /**
  * @ngdoc function
  * @name remittanceApp.controller:SendCtrl
@@ -9,6 +10,49 @@ var createRemittanceUrl = 'https://gateway.test.dinex.cl/public/remittance/creat
  * Controller of the remittanceApp
  */
 angular.module('remittanceApp')
-  .controller('SendCtrl', function ($scope, $http, _, store) {
+  .controller('SendCtrl', function ($scope, $http, _, store, $location, COLOMBIA_BANKS, COLOMBIA_BANK_ACCOUNT_TYPES, EMAIL_REGEX) {
+
+  // recuperar quotation
+  $scope.quotation = store.get('quotation');
+
+  if (!$scope.quotation) {
+    $location.path('#/');
+  }
+
+  $scope.colombiaBanks = COLOMBIA_BANKS;
+  $scope.colombiaBankAccountTypes = COLOMBIA_BANK_ACCOUNT_TYPES;
+  $scope.emailRegex = EMAIL_REGEX;
+
+  $scope.reset = function () {
+    $scope.remittanceInfo = false;
+    $scope.sourceOpts = {
+      email: '',
+      bankAccountHolderId: ''
+    };
+
+    $scope.destinationOpts = {
+      email: '',
+      bankAccountHolderId: '',
+      bankAccountHolderName: '',
+      bankAccountName: '',
+      bankAccountType: '',
+      bankAccountNumber: ''
+    };
+
+  };
+
+  $scope.reset();
+
+  $scope.setRemittanceInfo = function() {
+    $scope.remittanceInfo = true;
+    store.set('sourceOpts', $scope.sourceOpts);
+    var destinations = store.get('destinationOpts') ? store.get('destinationOpts') : [];
+    destinations.push($scope.destinationOpts);
+    store.set('destinationOpts', destinations);
+  };
+
+  $scope.create = function() {
+    // we create the remittance at gatewayd
+  };
 
   });
