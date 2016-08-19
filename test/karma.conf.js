40f2d5fcc60021d35b503eeef7c9ef7b1236fbe5
jaime-e <jechegarayz@gmail.com>
2016-08-19 17:12:11 -0300

    [fix] include non minified deps

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,6 +37,8 @@ module.exports = function(config) {
       'bower_components/moment/moment.js',
       'bower_components/humanize-duration/humanize-duration.js',
       'bower_components/angular-timer/dist/angular-timer.js',
+      'bower_components/clipboard/dist/clipboard.js',
+      'bower_components/ngclipboard/dist/ngclipboard.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
