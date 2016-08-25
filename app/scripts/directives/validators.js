276df20f4a3e194cb289b0a9e3e5b7d399efffac
jaime-e <jechegarayz@gmail.com>
2016-08-25 12:09:20 -0300

    [feature] directives to validate min and max numerical value for string input

--- app/scripts/directives/validators.js
+++ app/scripts/directives/validators.js
@@ -76,4 +76,80 @@ angular.module('remittanceApp')
       });
     }
   };
+})
+
+.directive('maxValue', function (_) {
+  /*
+  * validates a max numerical value for string input
+  */
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    link: function (scope, elm, attr, ctrl) {
+      if (!ctrl) {
+        return;
+      }
+
+      var validator = function(value) {
+        var valueAsNumber = value ? _.toNumber(value.replace(/,/g, '')) : 0;
+
+        if (attr.maxValue) {
+          if (valueAsNumber <= _.toNumber(attr.maxValue)) {
+            ctrl.$setValidity('maxValue', true);
+            return value;
+          } else {
+            ctrl.$setValidity('maxValue', false);
+            return value;
+          }
+        } else {
+          ctrl.$setValidity('maxValue', true);
+          return value;
+        }
+      };
+
+      ctrl.$formatters.push(validator);
+      ctrl.$parsers.unshift(validator);
+      attr.$observe('maxValue', function() {
+        validator(ctrl.$viewValue);
+      });
+    }
+  };
+})
+
+.directive('minValue', function (_) {
+  /*
+  * validates a min numerical value for string input
+  */
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    link: function (scope, elm, attr, ctrl) {
+      if (!ctrl) {
+        return;
+      }
+
+      var validator = function(value) {
+        var valueAsNumber = value ? _.toNumber(value.replace(/,/g, '')) : 0;
+
+        if (attr.minValue) {
+          if (valueAsNumber >= _.toNumber(attr.minValue)) {
+            ctrl.$setValidity('minValue', true);
+            return value;
+          } else {
+            ctrl.$setValidity('minValue', false);
+            return value;
+          }
+        } else {
+          ctrl.$setValidity('minValue', true);
+          return value;
+        }
+      };
+
+      ctrl.$formatters.push(validator);
+      ctrl.$parsers.unshift(validator);
+      attr.$observe('minValue', function() {
+        validator(ctrl.$viewValue);
+      });
+    }
+  };
 });
