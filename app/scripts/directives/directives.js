7284ad33cbd8ab8cf84c8d3cdf9f58b55076fdf0
jaime-e <jechegarayz@gmail.com>
2016-08-30 10:14:57 -0300

    [task] onError directive

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -50,4 +50,64 @@ angular.module('remittanceApp')
       });
     }
   };
+})
+
+.directive('onError', function() {
+  return {
+    restrict: 'AE',
+    link: function($scope, elem, attr) {
+
+      /** Usage:
+        The input group must be in a div with class="input-group" in order to use bootstrap classes
+        Fields must have a name property
+        on-error must be called with "fieldName:erorrType:message"
+        example <div on-error="srcAmnt:required:Debes llenar este campo">
+      **/
+
+      var fieldName = attr.onError.split(':')[0];
+      var errorName = attr.onError.split(':')[1];
+      var errorMessage = typeof attr.onError.split(':')[2] === 'undefined' ? 'error' : attr.onError.split(':')[2];
+      var formController = elem.inheritedData('$formController');
+      var formName = formController.$name;
+
+      function updateError() {
+        var field = formController[fieldName];
+        var fieldErrors = field.$error;
+
+        if (!field) {
+          return;
+        }
+
+        if (fieldErrors[errorName]) {
+          // error is activated
+          // add message if not already
+          if (elem[0].firstElementChild && elem[0].firstElementChild.id === 'helpBlock' + errorName) {
+            return;
+          } else {
+            elem.append('<span id="helpBlock' + errorName + '"' + 'class="help-block" style="">' + errorMessage + '</span>');
+          }
+          // set bootstrap class
+          if (elem[0].previousElementSibling && elem[0].previousElementSibling.className === 'input-group') {
+            elem[0].previousElementSibling.className = 'input-group has-error';
+          }
+
+
+        } else {
+          // remove message
+          if (elem[0].firstElementChild && elem[0].firstElementChild.id === 'helpBlock' + errorName) {
+            elem[0].firstElementChild.remove();
+          }
+          // and reset bootstrap class
+          if (elem[0].previousElementSibling && elem[0].previousElementSibling.className === 'input-group has-error') {
+            elem[0].previousElementSibling.className = 'input-group';
+          }
+
+        }
+      }
+
+      $scope.$watch(formName + '.' + fieldName + '.$error', updateError, true);
+      $scope.$watch(formName + '.' + fieldName + '.$pristine', updateError);
+    }
+  };
 });
+
